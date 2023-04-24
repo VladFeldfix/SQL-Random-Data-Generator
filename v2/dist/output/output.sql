cc61e463b6dbb1097b638d79610da128487d1a35
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2023-04-24 18:38:50 +0300

    general sync

---
+++ v2/dist/output/output.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO users (first_name, last_name, phone, sex) VALUES
+('Polonia', 'Axelrod', '050-9669482', 'F'),
+('Martiño', 'Kontges', '054-4950893', 'F'),
+('Colin', 'Lastovic', '054-1032776', 'M'),
+('Senobia', 'Hanlon', '054-3050639', 'M'),
+('Luciliano', 'Fieth', '052-3441082', 'F'),
+('Piera', 'De Miguel', '051-3867253', 'M'),
+('Chenyang', 'Swiatlowsk', '059-8582053', 'M'),
+('Vladyslava', 'Vanyushin', '054-6730789', 'F'),
+('Ares', 'Picapiedra', '050-9335717', 'M'),
+('Deanne', 'Piloo', '050-2195349', 'M');
+
